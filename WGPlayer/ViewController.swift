74fe1f7967d53d782857132a6869b4e1f84ae2a6
master <>
2020-03-18 11:49:59 +0800

    no message

---
+++ WGPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WGPlayer
+//
+//  Created by master on 2020/3/18.
+//  Copyright © 2020 master. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
